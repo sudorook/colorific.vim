ec95425e057cd5e53e9b43a16cc530791a8f9729
sudorook <daemon@nullcodon.com>
2018-06-06 11:17:37 -0400

    Add custom gitk styling.
    Copied over the one in https://github.com/dracula/gitk and tweaked the
    colors around a bit.

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ vimdir=${HOME}/.vim
 mkdir -p ${vimdir}/colors ${vimdir}/autoload/airline/themes
 cp -f colors/* ${vimdir}/colors/
 cp -f autoload/airline/themes/* ${vimdir}/autoload/airline/themes/
+
+gitkdir=${HOME}/.config/gitk
+mkdir -p ${gitkdir}
+cp -f gitk/gitk ${gitkdir}/
