b495a4c6f101995ab9fef32e727654bcb6c399b8
sudorook <daemon@nullcodon.com>
2018-06-06 22:18:29 -0400

    use verbose flags in install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ set -eu
 
 vimdir=${HOME}/.vim
 
-mkdir -p ${vimdir}/colors ${vimdir}/autoload/airline/themes
-cp -f colors/* ${vimdir}/colors/
-cp -f autoload/airline/themes/* ${vimdir}/autoload/airline/themes/
+mkdir -vp ${vimdir}/colors ${vimdir}/autoload/airline/themes
+cp -vf colors/* ${vimdir}/colors/
+cp -vf autoload/airline/themes/* ${vimdir}/autoload/airline/themes/
 
 gitkdir=${HOME}/.config/gitk
-mkdir -p ${gitkdir}
-cp -f gitk/gitk ${gitkdir}/
+mkdir -vp ${gitkdir}
+cp -vf gitk/gitk ${gitkdir}/
