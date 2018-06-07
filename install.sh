01809586e8b7f392c7f62dbc937a6390f8d56f25
sudorook <daemon@nullcodon.com>
2018-06-06 22:23:37 -0400

    fix install path; dim fg color to #cfd8dc

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ mkdir -vp ${vimdir}/colors ${vimdir}/autoload/airline/themes
 cp -vf colors/* ${vimdir}/colors/
 cp -vf autoload/airline/themes/* ${vimdir}/autoload/airline/themes/
 
-gitkdir=${HOME}/.config/gitk
-mkdir -vp ${gitkdir}
-cp -vf gitk/gitk ${gitkdir}/
+gitdir=${HOME}/.config/git
+mkdir -vp ${gitdir}
+cp -vf git/gitk ${gitdir}/
