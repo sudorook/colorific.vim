5c95a6e94f731733b38c98af35547b253682aeed
sudorook <daemon@nullcodon.com>
2022-01-15 13:35:26 -0600

    minor rewrite of palette swap scripts (fewer writes)

--- utils/convert-dark2-dark.sh
+++ utils/convert-dark2-dark.sh
@@ -1,17 +1,15 @@
 #! /bin/bash
 set -eu
 
-file="${1}"
-newfile="${1}.new"
-
-cp "$file" "$newfile"
+FILE="${1}"
+NEWFILE="${1%.*}_new.${1##*.}"
 
 # Dark2 -> Dark
-sed -i "s/e3e3e3/cfd8dc/g" "${newfile}"
-sed -i "s/1c1c1c/2e3c44/g" "${newfile}"
-sed -i "s/545454/607d8b/g" "${newfile}"
-sed -i "s/e4e4e4/b0b3c5/g" "${newfile}"
-sed -i "s/2e2e2e/37474f/g" "${newfile}"
-sed -i "s/d0d0d0/b0bec5/g" "${newfile}"
-sed -i "s/404040/435761/g" "${newfile}"
-# sed -i "s/404040/465B66/g" "${newfile}"
+sed \
+  -e "s/e3e3e3/cfd8dc/g" \
+  -e "s/1c1c1c/2e3c44/g" \
+  -e "s/545454/607d8b/g" \
+  -e "s/e4e4e4/b0b3c5/g" \
+  -e "s/2e2e2e/37474f/g" \
+  -e "s/d0d0d0/b0bec5/g" \
+  -e "s/404040/435761/g" "${FILE}" > "${NEWFILE}"
--- utils/convert-light2-light.sh
+++ utils/convert-light2-light.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 set -eu
 
-file="${1}"
-newfile="${1}.new"
-
-cp "$file" "$newfile"
+FILE="${1}"
+NEWFILE="${1%.*}_new.${1##*.}"
 
 # Light2 -> Light
-sed -i "s/949494/78909c/g" "${newfile}"
-sed -i "s/d9d9d9/bac5cc/g" "${newfile}"
-sed -i "s/2e2e2e/37474f/g" "${newfile}"
-sed -i "s/e3e3e3/cfd8dc/g" "${newfile}"
-sed -i "s/ededed/e3e8eb/g" "${newfile}"
-sed -i "s/f0f0f0/eceff1/g" "${newfile}"
-sed -i "s/545454/607d8b/g" "${newfile}"
+sed \
+  -e "s/949494/78909c/g" \
+  -e "s/d9d9d9/bac5cc/g" \
+  -e "s/2e2e2e/37474f/g" \
+  -e "s/e3e3e3/cfd8dc/g" \
+  -e "s/ededed/e3e8eb/g" \
+  -e "s/f0f0f0/eceff1/g" \
+  -e "s/545454/607d8b/g" "${FILE}" > "${NEWFILE}"
