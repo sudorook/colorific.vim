5ef3765ae310dc97955bc001dbb99bd00385e951
sudorook <daemon@nullcodon.com>
2022-01-17 16:27:19 -0600

    rewrite install script to specify file names and paths exactly

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 set -eu
 
+ROOT="$(dirname "${0}")"
+
 VIMDIR=${HOME}/.vim
 mkdir -vp "${VIMDIR}/colors" "${VIMDIR}/autoload/airline/themes"
-cp -vf colors/* "${VIMDIR}/colors/"
-cp -vf autoload/airline/themes/* "${VIMDIR}/autoload/airline/themes/"
+cp -vf "${ROOT}"/colors/colorific.vim "${VIMDIR}/colors/"
+cp -vf "${ROOT}"/autoload/airline/themes/colorific.vim "${VIMDIR}/autoload/airline/themes/"
 
 GITDIR=${HOME}/.config/git
 mkdir -vp "${GITDIR}"
-cp -vf git/gitk-* "${GITDIR}/"
+cp -vf "${ROOT}"/git/gitk-{light,light2,light3,dark,dark2,dark3} "${GITDIR}/"
 
 TMUXDIR=${HOME}/.tmux
 mkdir -vp "${TMUXDIR}"
-cp -vf tmux/* "${TMUXDIR}/"
+cp -vf "${ROOT}"/tmux/{light,light2,light3,dark,dark2,dark3}.tmuxtheme "${TMUXDIR}/"
